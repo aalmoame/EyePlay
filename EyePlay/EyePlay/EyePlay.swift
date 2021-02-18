627e05a7531853a08711c085fb3a016a3d33786d
aalmoame <aalmoame@umich.edu>
2021-02-18 16:14:20 -0500

    comments and storyboard updates

--- EyePlay/EyePlay/EyePlay.swift
+++ EyePlay/EyePlay/EyePlay.swift
@@ -10,6 +10,11 @@ import ARKit
 import VisionKit
 
 
+
+//constants and helper functions
+
+
+//simple helper functions for CGPoints
 extension CGPoint {
     func add(point: CGPoint) -> CGPoint {
         return CGPoint(x: self.x + point.x, y: self.y + point.y)
@@ -19,8 +24,10 @@ extension CGPoint {
         let denominator = CGFloat(by)
         return CGPoint(x: self.x / denominator, y: self.y / denominator)
     }
+    
 }
 
+//gets the average of an array (Collection)
 extension Collection where Element == CGPoint {
     func average() -> CGPoint {
         let point = self.reduce(CGPoint(x: 0, y: 0)) { (result, point) -> CGPoint in
@@ -30,6 +37,8 @@ extension Collection where Element == CGPoint {
     }
 }
 
+
+//sets boundaries for points
 extension CGFloat {
 
     func clamped(to: ClosedRange<CGFloat>) -> CGFloat {
@@ -37,8 +46,10 @@ extension CGFloat {
             : to.upperBound < self ? to.upperBound
             : self
     }
+
 }
 
+//constants for given device size and screen size
 struct Constants {
 
     struct Device {
@@ -46,6 +57,7 @@ struct Constants {
         static let frameSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
     }
 
+    
     struct Ranges {
         static let widthRange: ClosedRange<CGFloat> = (0...Device.frameSize.width)
         static let heightRange: ClosedRange<CGFloat> = (0...Device.frameSize.height)
@@ -53,14 +65,17 @@ struct Constants {
 
 }
 
+
+//main view class
 class EyePlay: UIViewController{
     
+    //Elements connected to the storyboard
     @IBOutlet var mainView: ARSCNView!
     @IBOutlet weak var cursor: UIImageView!
-    
     @IBOutlet weak var ballGameButton: UIButton!
 
-    
+
+    //returns a SCNNode corresponding to the left eye
     var leftEyeNode: SCNNode = {
             let geometry = SCNCone(topRadius: 0.005, bottomRadius: 0, height: 0.1)
             geometry.radialSegmentCount = 3
@@ -74,6 +89,7 @@ class EyePlay: UIViewController{
             return parentNode
         }()
 
+    //returns a SCNNode corresponding to the left eye
     var rightEyeNode: SCNNode = {
         let geometry = SCNCone(topRadius: 0.005, bottomRadius: 0, height: 0.1)
         geometry.radialSegmentCount = 3
@@ -87,12 +103,14 @@ class EyePlay: UIViewController{
         return parentNode
     }()
 
+    //boundaries for the the left eye
     var endPointLeftEye: SCNNode = {
         let node = SCNNode()
         node.position.z = 2
         return node
     }()
 
+    //boundaries for the the left eye
     var endPointRightEye: SCNNode = {
         let node = SCNNode()
         node.position.z = 2
@@ -111,42 +129,44 @@ class EyePlay: UIViewController{
             return node
         }()
     
+    //sets the view up
     override func viewWillAppear(_ animated: Bool) {
       super.viewWillAppear(animated)
             
-      // 1
       let configuration = ARFaceTrackingConfiguration()
-            
-      // 2
       mainView.session.run(configuration)
     }
-        
+    
+    
+    // pauses the view
     override func viewWillDisappear(_ animated: Bool) {
       super.viewWillDisappear(animated)
-            
-      // 1
       mainView.session.pause()
     }
     
+    //configures the screen once its loaded up
     override func viewDidLoad() {
-            super.viewDidLoad()
+        super.viewDidLoad()
 
-            guard ARFaceTrackingConfiguration.isSupported else {
-                fatalError("Face tracking is not supported on this device")
-            }
+        guard ARFaceTrackingConfiguration.isSupported else {
+            fatalError("Face tracking is not supported on this device")
+        }
         mainView.pointOfView?.addChildNode(nodeInFrontOfScreen)
         mainView.delegate = self
 
     }
     
+    //holds the recorded points of left and right eye nodes
     var points: [CGPoint] = []
     
     
+    //maps the left and right eye gaze positions from a 3-D planes (SCNNodes) to 2-D CGPoints
     func hitTest() {
 
             var leftEyeLocation = CGPoint()
             var rightEyeLocation = CGPoint()
 
+            //segment from where the actual eye is to where the end of its geometry is located (end of the cone)
             let leftEyeResult = nodeInFrontOfScreen.hitTestWithSegment(from: endPointLeftEye.worldPosition,
                                                               to: leftEyeNode.worldPosition,
                                                               options: nil)
@@ -155,12 +175,14 @@ class EyePlay: UIViewController{
                                                               to: rightEyeNode.worldPosition,
                                                               options: nil)
 
+            //checks if the eyes are detected and gazing somewhere on the screen
             if leftEyeResult.count > 0 || rightEyeResult.count > 0 {
 
                 guard let leftResult = leftEyeResult.first, let rightResult = rightEyeResult.first else {
                     return
                 }
 
+                //mapping the position to a 2-D plane
                 leftEyeLocation.x = CGFloat(leftResult.localCoordinates.x) / (Constants.Device.screenSize.width / 2) *
                     Constants.Device.frameSize.width
                 leftEyeLocation.y = CGFloat(leftResult.localCoordinates.y) / (Constants.Device.screenSize.height / 2) *
@@ -171,6 +193,7 @@ class EyePlay: UIViewController{
                 rightEyeLocation.y = CGFloat(rightResult.localCoordinates.y) / (Constants.Device.screenSize.height / 2) *
                     Constants.Device.frameSize.height
 
+                //converging the two values to one single point
                 let point: CGPoint = {
                     var point = CGPoint()
                     let pointX = ((leftEyeLocation.x + rightEyeLocation.x) / 2)
@@ -184,15 +207,17 @@ class EyePlay: UIViewController{
                 points.append(point)
                 points = points.suffix(50).map {$0}
 
+                //placing the cursor at the center of that point
                 DispatchQueue.main.async {
                     UIView.animate(withDuration: 0.05, animations: {
                         self.cursor.center = self.points.average()
                     })
                 }
-                
             }
         }
     
+    
+    //checks if the cursor is on top of the game button and if the user blinks
     func collision(faceAnchor: ARFaceAnchor){
         
         
@@ -202,23 +227,21 @@ class EyePlay: UIViewController{
         if cursor.frame.intersects(ballGameButton.frame) &&
             eyeBlinkValue > 0.5 {
             
+            //go to game screen when user blinks over button
             DispatchQueue.main.async {
                 self.performSegue(withIdentifier: "BallGameSegue", sender: self)
             }
-
-            
         }
-                
     }
     
 }
 
-// 1
 extension EyePlay: ARSCNViewDelegate {
-  // 2
+    
+    //a scene renderer that returns a scene node given a face anchor, runs once
+    
       func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
         
-        // 3
         guard let device = mainView.device else {
           return nil
         }
@@ -235,18 +258,19 @@ extension EyePlay: ARSCNViewDelegate {
         return node
       }
     
+    /*this renderer takes as input the previous renderer's scene node, and runs continuously
+     with that node given the value has changed, if yes, then it uses that new node */
+    
     func renderer(
       _ renderer: SCNSceneRenderer,
       didUpdate node: SCNNode,
       for anchor: ARAnchor) {
        
-      // 2
       guard let faceAnchor = anchor as? ARFaceAnchor,
         let faceGeometry = node.geometry as? ARSCNFaceGeometry else {
           return
       }
         
-      // 3
         leftEyeNode.simdTransform = faceAnchor.leftEyeTransform
         rightEyeNode.simdTransform = faceAnchor.rightEyeTransform
 
